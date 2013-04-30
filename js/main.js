2a4e8203e184e28dcc14b75398b78ab4f96526a1
clindsay107 <clindsay107@gmail.com>
2013-04-30 16:09:20 -0700

    Added collapsable RSS reader

--- js/main.js
+++ js/main.js
@@ -2,4 +2,32 @@ window.onload = function(){
   var text_input = document.getElementById ('searchBox');
   text_input.focus ();
   text_input.select ();
+
+  $('#rss_title').click(function() {
+  	if ($('#rss_title').html() == 'HN RSS - show') {
+  		$('#rss_title').html('HN RSS - hide');
+  	} else {
+  		$('#rss_title').html('HN RSS - show');
+  	}
+	$('.rss ul').slideToggle('slow');
+});
 }
+
+var rss = 'https://news.ycombinator.com/rss';
+
+$(function(){
+	$.ajax({
+		type: "GET",
+		url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(rss),
+		dataType: 'json',
+		error: function(){
+		    alert('Unable to load RSS feed, Incorrect path or invalid feed :(');
+		},
+		success: function(xml){
+		    values = xml.responseData.feed.entries;
+		    $.each(values, function() {
+		    	$('.rss ul').append('<li><a href="' + this.link + '">' + this.title + '</a> - ' + this.content + '</li>');
+		    });
+		}
+	});
+});
