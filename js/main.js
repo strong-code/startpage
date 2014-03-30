aa3edd3b2eba4f0b045f1078c4e66d12f2bc4c56
clindsay107 <clindsay107@gmail.com>
2014-03-30 15:23:48 -0700

    menu on mouseover

--- js/main.js
+++ js/main.js
@@ -1,33 +1,13 @@
-window.onload = function(){
-  var text_input = document.getElementById ('searchBox');
-  text_input.focus ();
-  text_input.select ();
-
-  $('#rss_title').click(function() {
-  	if ($('#rss_title').html() == 'HN RSS v show') {
-  		$('#rss_title').html('HN RSS ^ hide');
-  	} else {
-  		$('#rss_title').html('HN RSS v show');
-  	}
-	$('.rss ul').slideToggle('slow');
-});
-}
-
-var rss = 'https://news.ycombinator.com/rss';
-
-$(function(){
-	$.ajax({
-		type: "GET",
-		url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(rss),
-		dataType: 'json',
-		error: function(){
-		    alert('Unable to load RSS feed, Incorrect path or invalid feed :(');
-		},
-		success: function(xml){
-		    values = xml.responseData.feed.entries;
-		    $.each(values, function() {
-		    	$('.rss ul').append('<li><a href="' + this.link + '">' + this.title + '</a> - ' + this.content + '</li>');
-		    });
+$(document).ready(function() {
+	$('.link-tile').on('mouseover', function(e) {
+		if ($(e.currentTarget).hasClass("sub-menu")) {
+			$('#submenu').css("display", "block");
 		}
 	});
-});
+
+	$('.link-tile').on('mouseover', function(e) {
+		if (!$(e.currentTarget).hasClass("sub-menu")) {
+			$('#submenu').css("display", "none");
+		}
+	});
+})
