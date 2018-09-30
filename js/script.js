c662ae4093b810f509780cfcfc0f4709d70b6b71
clindsay107 <clindsay107@gmail.com>
2018-09-30 00:22:14 -0700

    fix slickdeals domain to avoid ssl error

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ var linkMenu = [
   ["CoinMarketCap",            "https://coinmarketcap.com",""],
   ["eBay",                     "https://ebay.com",""],
   ["Pivotal Tracker",          "https://www.pivotaltracker.com/n/projects/2187004",""],
-  ["SlickDeals",               "https://www.slickdeals.com",""],
+  ["SlickDeals",               "https://slickdeals.net/",""],
 ];
 // DID I FORGET TO MENTION?! THE DEMO LINKS DO NOTHING!
 
